721a2a3f327d1ece7c22c38171d84ef963e76732
javier <74383159+quieroSer@users.noreply.github.com>
2022-01-28 10:07:07 -0300

    Create index.ts

---
+++ Learn-TS/park-service/index.ts
@@ -0,0 +1,86 @@
+import {
+  RaccoonMeadowsVolunteers,
+  RaccoonMeadowsActivity,
+  raccoonMeadowsVolunteers,
+} from './raccoon-meadows-log';
+
+import {
+  WolfPointVolunteers,
+  WolfPointActivity,
+  wolfPointVolunteers,
+} from './wolf-point-log';
+
+type CombinedActivity = RaccoonMeadowsActivity | WolfPointActivity;
+
+type Volunteers = {
+  id: number;
+  name: string;
+  activities: CombinedActivity[];
+};
+
+function combineVolunteers(
+  volunteers: (RaccoonMeadowsVolunteers | WolfPointVolunteers)[]
+) {
+  return volunteers.map( v => {
+    let id = v.id
+    if(typeof id === 'string') {
+      id = parseInt(id, 10)
+    }
+    return {
+      id: id,
+      name: v.name,
+      activities: v.activities
+    }
+  })
+}
+
+function isVerified(verified: string | boolean) {
+  if(typeof verified === 'string') {
+    if(verified === 'Yes') {
+      return true
+    } else {
+      return false
+    }
+  }
+  return verified
+}
+
+function getHours(activity: CombinedActivity) {
+  if('hours' in activity) {
+    return activity.hours
+  } else {
+    return activity.time
+  }
+}
+
+function calculateHours(volunteers: Volunteers[]) {
+  return volunteers.map((volunteer) => {
+    let hours = 0;
+
+    volunteer.activities.forEach((activity) => {
+      if(isVerified(activity.verified)) {
+        hours += getHours(activity)
+      }
+    });
+
+    return {
+      id: volunteer.id,
+      name: volunteer.name,
+      hours: hours,
+    };
+  });
+}
+
+function byHours(a, b) {
+  return b.hours - a.hours
+}
+
+
+const combinedVolunteers = combineVolunteers(
+  [].concat(wolfPointVolunteers, raccoonMeadowsVolunteers)
+);
+
+//console.log(combinedVolunteers)
+let result = calculateHours(combinedVolunteers)
+console.log(result)
+console.log(result.sort(byHours))
